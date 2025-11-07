38e05baa7bc9005bf4887d0bf77c20ae387c0dcb
Sherwani Stagg <sherwani.stagg@gmail.com>
2025-11-07 20:52:35 +0000

    Create hello_world.py
    Added Python code for day one of the 100-day challenge.

---
+++ day01/hello_world.py
@@ -0,0 +1,8 @@
+print("Hello World!")
+print("""Sherwani
+7th November 2025
+I'm signing up for Replit's 100 days of Python challenge!
+I'll make sure to spend some time every day coding along, for a minimum of 10 minutes a day.
+I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happen to be. No excuses for not coding from the middle of a field!
+I am feeling 🥳!
+You can follow my progress at replit.com/@wanistagg""")
