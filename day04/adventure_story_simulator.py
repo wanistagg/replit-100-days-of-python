9ed36068d7c4d29a90eb4e981eb5ebc74cb784d9
Sherwani Stagg <sherwani.stagg@gmail.com>
2025-11-10 23:46:58 +0000

    Create adventure_story_simulator.py
    Added code for day four of the 100 days of code challenge.

---
+++ day04/adventure_story_simulator.py
@@ -0,0 +1,46 @@
+print("=== Your Very Own Adventure Simulator ===")
+print()
+
+print("""Welcome to Your Very Own Adventure Simulator. You will be asked a set of questions which will be used to generate a fun story featuring YOU as the main character!
+Answer the questions below to get started!""")
+print()
+
+name = input("What is your name?: ")
+weather = input("How is the weather today?: ")
+famousPerson = input("Who is your favourite famous person?: ")
+place = input("Choose a place you would find in a town: ")
+clothing = input("Choose a piece of clothing: ")
+dislikedColour = input("What is your least favourite colour?: ")
+sport = input("Choose a sport: ")
+food = input("Choose a food: ")
+otherFood = input("Choose another food: ")
+verb = input("Choose a verb ending in 'ing': ")
+animal = input("Choose an animal: ")
+print()
+
+print("=== Your Story! ===")
+print()
+
+print("One", weather, "day,", "there was a person called", name, " who was about to set off into town to enjoy their day off. They were on their way to meet", famousPerson, ", their friend, at the", place, "to catch up. It had been a long time since they last saw each other.", famousPerson, "was wearing a", dislikedColour, clothing, ", which", name, "thought looked rather fashionable.", famousPerson, "had just finished playing", sport, "before arriving at the", place, ".")
+print()
+
+print("They were both feeling hungry, so they decided to go and have lunch together. 'Want to try some", food, "? It's my favourite!',", famousPerson, "said.")
+print()
+      
+print("'No thanks,", name, "replied. 'I'm not a fan of", food, ". 'How about we go for some", otherFood, "instead?',", name, "suggested.")
+print()
+
+print("'Sure! Let's go have that then',", famousPerson, "said. On their way to the restaurant, they saw a", animal, verb, "and they laughed.", name, "had a great time enjoying their", otherFood, "with", famousPerson, ", and the two caught up on all the latest news since the last time they met.")
+print()
+
+print("After lunch,", name, "parted ways with", famousPerson, ", and saw the", animal, "which was", verb, "just earlier on the way to the restaurant.", name, "took out their phone to take a photo as proof of the amusing encounter, when all of a sudden, they heard a 'hello' out of nowhere.")
+print()
+
+print("'Who was that?',", name, "asked.")
+print("'It's me', the", animal, "said. 'I saw you going to the restaurant with", famousPerson, "earlier. Did you see that", dislikedColour, clothing, "? I thought it looked cool!', the", animal, "exclaimed.")
+print()
+
+print("Shocked beyond belief,", name, "simply stared at the", animal, ", before shrugging off everything that their eyes and ears had just witnessed. 'Did you know", famousPerson, "always plays", sport, "on days like this? I was supposed to join them, but felt like", verb, "instead. Hey, how about you come and join me?'")
+print()
+
+print(" With not much else to do for the rest of the day,", name, ", after a rather long pause, said 'Sure! Why not. I've got plenty of time to kill, anyway.' The", animal, ", excited they found a new friend, went on to spend the rest of the afternoon", verb, "together with", name, ", who found the whole experience incredibly funny.", name, "went on to spend a more than fun rest of the day together with their new animal pal.")
